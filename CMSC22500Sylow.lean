5425b09cdd4ef1951d32dfd4d10556b307d8c0e6
Hadrian <hadrian@uchicago.edu>
2024-05-01 11:41:20 -0500

    initial commit

---
+++ CMSC22500Sylow.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `CMSC22500Sylow` library.
+-- Import modules here that should be built as part of the library.
+import «CMSC22500Sylow».Basic
---
+++ lakefile.lean
@@ -0,0 +1,16 @@
+import Lake
+open Lake DSL
+
+package «CMSC-22500-Sylow» where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+  -- add any additional package configuration options here
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «CMSC22500Sylow» where
+  -- add any library configuration options here
