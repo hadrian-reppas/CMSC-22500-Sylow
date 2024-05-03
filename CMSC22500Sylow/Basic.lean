563fbebe26141f139b39095168cc6c987a125210
Hadrian <hadrian@uchicago.edu>
2024-05-03 17:30:16 -0500

    done with subgroup proof

--- CMSC22500Sylow/Basic.lean
+++ CMSC22500Sylow/Basic.lean
@@ -2,25 +2,27 @@ import Mathlib.Data.Nat.Prime
 import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup
 import Mathlib.RingTheory.LittleWedderburn
 import Mathlib.Init.Order.Defs
+import Mathlib.LinearAlgebra.Matrix.Block
 
-def GLₙFₚ (n p : ℕ) := GL (Fin n) (ZMod p)
+abbrev GLₙFₚ (n p : ℕ) := GL (Fin n) (ZMod p)
 
-variable {n p : ℕ}
+variable {n p : ℕ} {M N : GLₙFₚ n p}
 
 -- `GLₙFₚ n p` is a group
 instance GLₙFₚGroup : Group (GLₙFₚ n p) := Units.instGroup
 
+def ZerosUnderDiag (M : GLₙFₚ n p) : Prop := ∀ i j, j < i → M.val i j = 0
+def OnesOnDiag (M : GLₙFₚ n p) : Prop := ∀ i, M.val i i = 1
+
 -- A predicate for upper triangular matrices
-def IsUpperTriangular (M : GLₙFₚ n p) : Prop :=
-  (∀ i j, j < i → M.val i j = 0) ∧ (∀ i, M.val i i = 1)
+def IsUpperTriangular (M : GLₙFₚ n p) : Prop := ZerosUnderDiag M ∧ OnesOnDiag M
 
 -- The product of two ut matrices has zeros under the diagonal
-lemma ut_mul_zeros {a b : GLₙFₚ n p}
-  (ha : IsUpperTriangular a) (hb : IsUpperTriangular b) (i j : Fin n) (h : j < i)
-   : Matrix.dotProduct (λ k ↦ a.val i k) (λ k ↦ b.val k j) = 0 :=
+lemma ut_mul_zeros (hM : IsUpperTriangular M) (hN : IsUpperTriangular N) (i j : Fin n) (h : j < i)
+   : Matrix.dotProduct (λ k ↦ M.val i k) (λ k ↦ N.val k j) = 0 :=
   Finset.sum_eq_zero (λ k _ ↦
-    if hki : k < i then by simp [ha.left i k hki]
-    else by simp [hb.left k j (lt_of_lt_of_le h (not_lt.mp hki))])
+    if hki : k < i then by simp [hM.left i k hki]
+    else by simp [hN.left k j (lt_of_lt_of_le h (not_lt.mp hki))])
 
 lemma ne_lt_or_ge {a b : Fin n} (h : a ≠ b) : a < b ∨ a > b :=
   match le_or_gt a b with
@@ -29,30 +31,50 @@ lemma ne_lt_or_ge {a b : Fin n} (h : a ≠ b) : a < b ∨ a > b :=
     | .inr h₂ => absurd h₂ h
   | .inr h₁ => .inr h₁
 
-lemma ne_prod_0 {i j : Fin n} {a b : GLₙFₚ n p}
-  (ha : IsUpperTriangular a) (hb : IsUpperTriangular b) (h : j ≠ i)
-   : (a.val i j) * (b.val j i) = 0 :=
+lemma zud_prod_0 {i j : Fin n} (hM : ZerosUnderDiag M) (hN : ZerosUnderDiag N) (h : j ≠ i)
+   : (M.val i j) * (N.val j i) = 0 :=
     match ne_lt_or_ge h with
-    | .inl h => by simp [ha.left i j h]
-    | .inr h => by simp [hb.left j i h]
+    | .inl h => by simp [hM i j h]
+    | .inr h => by simp [hN j i h]
 
 -- The product of two ut matrices has ones on the diagonal
-lemma ut_mul_ones {a b : GLₙFₚ n p}
-  (ha : IsUpperTriangular a) (hb : IsUpperTriangular b) (i : Fin n)
-   : Matrix.dotProduct (λ k ↦ a.val i k) (λ k ↦ b.val k i) = 1 := by simp [
+lemma ut_mul_ones (hM : IsUpperTriangular M) (hN : IsUpperTriangular N) (i : Fin n)
+   : Matrix.dotProduct (λ k ↦ M.val i k) (λ k ↦ N.val k i) = 1 := by simp [
       Matrix.dotProduct,
       Finset.sum_eq_single_of_mem i
         (Finset.mem_univ i)
-        (λ _ _ h ↦ ne_prod_0 ha hb h),
-      ha.right i, hb.right i
+        (λ _ _ h ↦ zud_prod_0 hM.left hN.left h),
+      hM.right i, hN.right i
     ]
 
--- The inverse of an ut matrix has zeros under the diagonal
-lemma ut_inv_zeros {x : GLₙFₚ n p} {i j : Fin n} (h₁ : IsUpperTriangular x)
-   (h₂ : j < i) : x.inv i j = 0 := sorry
+instance : Invertible M.val := Units.invertible M
+
+lemma zud_block_triangular (hM : ZerosUnderDiag M) : Matrix.BlockTriangular M.val id := hM
+
+-- The inverse of an zud matrix is a zud matrix
+lemma zud_inv_zud (h₁ : ZerosUnderDiag M) : ZerosUnderDiag M⁻¹ :=
+  λ i j h₂ ↦ by
+    simp [Matrix.coe_units_inv M]
+    exact (Matrix.blockTriangular_inv_of_blockTriangular (zud_block_triangular h₁) h₂)
 
 -- The inverse of an ut matrix has ones on the diagonal
-lemma ut_inv_ones {x : GLₙFₚ n p} {i : Fin n} (h₁ : IsUpperTriangular x) : x.inv i i = 1 := sorry
+lemma ut_inv_ones {i : Fin n} (h : IsUpperTriangular M) : M.inv i i = 1 :=
+  have h₁ : (M.inv * M) i i = 1 := by simp [M.inv_val, Matrix.one_apply_eq i]
+  have h₂ : Matrix.dotProduct (λ k ↦ M.inv i k) (λ k ↦ M k i) = 1 := h₁
+  have h₃ : Matrix.dotProduct (λ k ↦ M.inv i k) (λ k ↦ M k i) = M.inv i i * M i i :=
+    Finset.sum_eq_single_of_mem i
+      (Finset.mem_univ i)
+      (λ j _ h₁ ↦ by
+        have h' := zud_prod_0 (zud_inv_zud h.left) h.left h₁
+        simp at *
+        exact h')
+  have h₄ := h₃.symm.trans h₂
+  have h₅ : M.val i i = 1 := h.right i
+  have h₆ : M.inv i i = 1 := by
+    rw [h₅] at h₄
+    simp at *
+    exact h₄
+  h₆
 
 -- The subgroup of upper triangular matrices
 def UpperTriangularₙₚ (n : ℕ) (p : ℕ) : Subgroup (GLₙFₚ n p) :=
@@ -63,8 +85,5 @@ def UpperTriangularₙₚ (n : ℕ) (p : ℕ) : Subgroup (GLₙFₚ n p) :=
       λ _ _ h ↦ Matrix.one_apply_ne (Ne.symm (Fin.ne_of_lt h)),
       Matrix.one_apply_eq
     ⟩,
-    inv_mem' := λ h₁ ↦ ⟨λ _ _ h₂ ↦ ut_inv_zeros h₁ h₂, λ _ ↦ ut_inv_ones h₁⟩
+    inv_mem' := λ h ↦ ⟨zud_inv_zud h.left, λ _ ↦ ut_inv_ones h⟩
   }
-
-def GL₃F₅Group := @GLₙFₚGroup 3 5
-#eval GL₃F₅Group.one -- the 3x3 identity matrix
