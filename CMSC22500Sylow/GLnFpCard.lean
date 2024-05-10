ddaca1646015bcaeb1d4ca3182e180f7dcbadb02
Hadrian <hadrian@uchicago.edu>
2024-05-09 20:32:06 -0500

    link

--- CMSC22500Sylow/GLnFpCard.lean
+++ CMSC22500Sylow/GLnFpCard.lean
@@ -1,5 +1,6 @@
 import CMSC22500Sylow.GLnFp
 
+-- https://people.math.osu.edu/cueto.5/teaching/6111/Au20/files/HW03Solutions.pdf
 -- https://leanprover-community.github.io/mathlib4_docs/Mathlib/GroupTheory/Coset.html#Subgroup.card_subgroup_dvd_card
 -- https://leanprover-community.github.io/mathlib4_docs/Mathlib/Data/Fintype/Perm.html#Fintype.card_perm
 -- https://leanprover-community.github.io/mathlib4_docs/Mathlib/LinearAlgebra/LinearIndependent.html
